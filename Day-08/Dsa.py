2f5f4d21c25189aaabe571eec1abbf83dcdf701f
Manoj Kumar K <manojkumar866013@gmail.com>
2026-08-31 21:22:11 +0530

    Complete Day 8 DSA

---
+++ Day-08/Dsa.py
@@ -0,0 +1,9 @@
+#Two sum
+nums = [2, 7, 11, 15]
+target = 13
+dit={}
+for i ,n in enumerate(nums):
+    required=target-n
+    if required in dit:
+        print(f'{dit[required]},{i}')
+    dit[n]=i
